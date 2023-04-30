65f0888d7cebb71472e7ec189d0290a67a8c249e
tonda28 <kuznik.a@bezmerov.net>
2023-04-30 06:18:58 +0200

    Tect Operation setSummaryTableAccordingToIndex

--- src/main/scala/Workbench/One.scala
+++ src/main/scala/Workbench/One.scala
@@ -1,9 +1,16 @@
 package Workbench
 
+import Database.ModelTableSummary
+
 import java.time.{Duration, LocalDate}
+import scala.collection.mutable.ListBuffer
 import scala.util.Try
 
-object One extends App with Utility.Pointer {
+object One
+    extends App
+    with Utility.Pointer
+    with Utility.Validator
+    with Utility.Calculator {
 
   val a = Simulator.IndexTable.apply(
     (2022, 10, 1),
@@ -12,9 +19,32 @@ object One extends App with Utility.Pointer {
   )
   val obj = new Simulator.DataSimulator()
   val t = obj.simulateIndex(a)
-  val tableSwitch = t._1.toList
-  val tableSummary = t._2.toList
+  val tableSwitch = t._1
+  val tableSummary = t._2
 
-  println(point(tableSummary, tableSwitch))
+  //println(point(tableSummary, tableSwitch))
+
+  //val dt = LocalDate.of(2022, 10, 2)
+  def sum(date: LocalDate, data: Seq[Database.ModelTableSwitch]) = {
+    val a = validateSingleDay(date, data)
+    val b = calculate(a)
+    b
+
+  }
+
+  def Iter() = {
+    val a = point(tableSummary, tableSwitch)
+    var l = ListBuffer[ModelTableSummary]()
+    for (item <- a) {
+      l += ModelTableSummary(
+        localdate = item._2,
+        duration = sum(item._2, tableSwitch)
+      )
+
+    }
+    l.toSeq
+  }
+
+   println(Iter())
 
 }
